be5956b8242e8d872ff177d6bc0dc3509d014731
CristianoSantan <cristiano.mer@gmail.com>
2024-10-22 20:31:29 -0300

    Configuração Rspec, Capybara e I18n

---
+++ spec/system/user_view_homepage_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe "Usuário visita tela inicial" do
+  it "e vê o nome da app" do
+    
+    visit root_path
+
+    expect(page).to have_content 'PaLeva'
+  end
+end
