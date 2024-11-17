3ef92116f40c7b8b9c55710951102a3c0a57296a
CristianoSantan <cristiano.mer@gmail.com>
2024-11-17 07:18:12 -0300

    API - marca pedido como pronto

--- spec/requests/apis/orders_api_spec.rb
+++ spec/requests/apis/orders_api_spec.rb
@@ -149,7 +149,7 @@ describe "Order API" do
   end
   
   context "PATCH /api/v1/orders/:establishment_code/:order_code" do
-    it "update status of order" do
+    it "update status of order for in_preparation" do
       user = User.create!(name: 'Freddie Mercury', cpf: CPF.generate, email: 'freddie@burgerqueen.com', password: 'password1234')
       establishment = Establishment.create!(
         brand_name: 'BurgerQueen',
@@ -180,6 +180,39 @@ describe "Order API" do
       json_response = JSON.parse(response.body)
       expect(json_response.class).to eq Hash
       expect(json_response["status"]).to eq "in_preparation"
+    end
+
+    it "update status of order for ready" do
+      user = User.create!(name: 'Freddie Mercury', cpf: CPF.generate, email: 'freddie@burgerqueen.com', password: 'password1234')
+      establishment = Establishment.create!(
+        brand_name: 'BurgerQueen',
+        company_name: 'BurgerQueen Rock n\' Roll Restaurants',
+        cnpj: CNPJ.generate,
+        full_address: 'Rua Rock, 280',
+        phone: '11987654321',
+        email: 'contact@burgerqueen.com',
+        code: 'QUEEN01',
+        user: user
+      )
+      menu_rock_classics = Menu.create!(name: 'Rock Classics', establishment: establishment)
+      dish = Dish.create!(name: 'Bohemian Rhapsody Burger', description: 'Burger com carne de angus, queijo cheddar, alface, tomate e molho especial', calories: 850, establishment: establishment, status: 'enabled')
+      portion_dish_1 = Portion.create!(description: 'Porção pequena', real: 12, cent: 0, portionable: dish)
+      portion_dish_2 = Portion.create!(description: 'Porção grande', real: 20, cent: 0, portionable: dish)
+      MenuItem.create!(menu: menu_rock_classics, menuable: dish)
+      order = Order.create!(name: 'José', phone: '1122332233', email: 'jose@email', cpf: CPF.generate,
+      code: 'ABCD1234', status: 'in_preparation', establishment: establishment)
+      order_item = OrderItem.create!(order: order, orderable:dish, portion: portion_dish_1, note: 'sem cebola')
+      order2 = Order.create!(name: 'Maria', phone: '1144554455', email: 'maria@email', cpf: CPF.generate,
+      code: 'ABCD5678', status: 'ready', establishment: establishment)
+      order_item2 = OrderItem.create!(order: order2, orderable:dish, portion: portion_dish_1, note: 'com cebola')
+
+      patch "/api/v1/orders/QUEEN01/#{order.code}/?status=ready"
+
+      expect(response.status).to eq 201
+      expect(response.content_type).to include('application/json')
+      json_response = JSON.parse(response.body)
+      expect(json_response.class).to eq Hash
+      expect(json_response["status"]).to eq "ready"
     end
   end
   
